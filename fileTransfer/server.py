3b10eac6f739edc02b8c41acc98a51c89c5f75ce
Luca Olivieri <luca.olivieri-1@unitn.it>
2020-04-12 17:59:45 +0200

    Test simple python file transfer

---
+++ fileTransfer/server.py
@@ -0,0 +1,21 @@
+import socket               # Import socket module
+
+s = socket.socket()         # Create a socket object
+host = socket.gethostname()  # Get local machine name
+port = 12345                 # Reserve a port for your service.
+s.bind((host, port))        # Bind to the port
+f = open('torecv.png', 'wb')
+s.listen(5)                 # Now wait for client connection.
+while True:
+    c, addr = s.accept()     # Establish connection with client.
+    print('Got connection from', addr)
+    print("Receiving...")
+    listener = c.recv(1024)
+    while (listener):
+        print("Receiving...")
+        f.write(listener)
+        listener = c.recv(1024)
+    f.close()
+    print("Done Receiving")
+    c.send("Thank you for connecting".encode())
+    c.close()
