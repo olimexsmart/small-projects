bc3cce343fe24e12cde18c4cbf454be04495bbde
Luca Olivieri <luca.olivieri-1@unitn.it>
2020-05-20 21:54:51 +0200

    more version of same stuff

--- fileTransfer/client.py
+++ fileTransfer/client.py
@@ -1,12 +1,12 @@
 import socket               # Import socket module
 
 s = socket.socket()         # Create a socket object
-host = socket.gethostname()  # Get local machine name
+host = "192.168.1.129"  # Get local machine name
 port = 12345                 # Reserve a port for your service.
 
 s.connect((host, port))
 # s.send("Hello server!".encode())
-f = open('test.png', 'rb')
+f = open('torecv.mp4', 'rb')
 print('Sending...')
 listener = f.read(1024)
 while (listener):
--- fileTransfer/server.py
+++ fileTransfer/server.py
@@ -1,10 +1,10 @@
 import socket               # Import socket module
 
 s = socket.socket()         # Create a socket object
-host = socket.gethostname()  # Get local machine name
+host = "0.0.0.0"  # Get local machine name
 port = 12345                 # Reserve a port for your service.
 s.bind((host, port))        # Bind to the port
-f = open('torecv.png', 'wb')
+f = open('torecv.mp4', 'wb')
 s.listen(5)                 # Now wait for client connection.
 while True:
     c, addr = s.accept()     # Establish connection with client.
